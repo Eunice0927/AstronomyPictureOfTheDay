3c3a8f0a5dc2337ef373e188a5a82b8c4498c43d
Eunice <jes.e.0927@gmail.com>
2023-12-04 22:50:05 +0900

    ✨Feat: Add NavigationView to the outtermost view

--- AstronomyPictureOfTheDay/ContentView.swift
+++ AstronomyPictureOfTheDay/ContentView.swift
@@ -15,57 +15,59 @@ struct ContentView: View {
     @State var tag: Int?
     
     var body: some View {
-        ZStack {
-            NavigationLink(destination: DetailView(), tag: 1, selection: self.$tag) {
-                DetailView()
-            }
-            
-            GeometryReader { geo in
-                Image("blueAndPurpleGalaxyByJeremyThomas")
-                    .resizable()
-                    .aspectRatio(contentMode: .fill)
-                    .offset(x: isMoving ? -700 : 0, y: 0)
-                    .animation(.linear(duration: 20).repeatForever(), value: isMoving)
-                    .ignoresSafeArea()
-            }
-            
-            VStack {
-                Spacer()
-                
-                Text("Select a date of \n when you want to see \n the universe")
-                    .font(.system(size: 30, weight: .heavy))
-                    .foregroundStyle(.white)
-                
-                DatePicker("", selection: $selectedDate, in: ...Date(), displayedComponents: .date)
-                    .datePickerStyle(WheelDatePickerStyle())
-                    .labelsHidden()
-                    .colorInvert()
-                
-                Spacer().frame(height: 50)
-                    
-                Button {
-                    formattedDate = setDate(selectedDate)
-                    nasaApi.fetchData(formattedDate)
-                    self.tag = 1
-                } label: {
-                    Text("Go")
-                        .frame(width: 83, height: 47)
-                        .background(.black)
-                        .clipShape(RoundedRectangle(cornerRadius: 12))
-                        .foregroundStyle(.white)
-                        .opacity(0.54)
-                        .font(.system(size: 20, weight: .bold))
+        NavigationView {
+            ZStack {
+                NavigationLink(destination: DetailView(), tag: 1, selection: self.$tag) {
+                    DetailView()
                 }
                 
-                Spacer().frame(height: 100)
-            } //VStack
-            .onAppear() {
-                isMoving = true
-            }
-            .onDisappear() {
-                isMoving = false
-            }
-        } //ZStack
+                GeometryReader { geo in
+                    Image("blueAndPurpleGalaxyByJeremyThomas")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .offset(x: isMoving ? -700 : 0, y: 0)
+                        .animation(.linear(duration: 20).repeatForever(), value: isMoving)
+                        .ignoresSafeArea()
+                }
+                
+                VStack {
+                    Spacer()
+                    
+                    Text("Select a date of \n when you want to see \n the universe")
+                        .font(.system(size: 30, weight: .heavy))
+                        .foregroundStyle(.white)
+                    
+                    DatePicker("", selection: $selectedDate, in: ...Date(), displayedComponents: .date)
+                        .datePickerStyle(WheelDatePickerStyle())
+                        .labelsHidden()
+                        .colorInvert()
+                    
+                    Spacer().frame(height: 50)
+                    
+                    Button {
+                        formattedDate = setDate(selectedDate)
+                        nasaApi.fetchData(formattedDate)
+                        self.tag = 1
+                    } label: {
+                        Text("Go")
+                            .frame(width: 83, height: 47)
+                            .background(.black)
+                            .clipShape(RoundedRectangle(cornerRadius: 12))
+                            .foregroundStyle(.white)
+                            .opacity(0.54)
+                            .font(.system(size: 20, weight: .bold))
+                    }
+                    
+                    Spacer().frame(height: 100)
+                } //VStack
+                .onAppear() {
+                    isMoving = true
+                }
+                .onDisappear() {
+                    isMoving = false
+                }
+            } //ZStack
+        } //NavigationView
     }
     
     func setDate(_ selectedDate: Date) -> String {
