077ead98ffe738482b44b155fe29da7389254812
Eunice <jes.e.0927@gmail.com>
2023-12-04 22:55:24 +0900

    ✨Feat: Set DetailView
    - Make Background black
    - Get api data: title, imageurl, explanation, copyright

--- AstronomyPictureOfTheDay/DetailView.swift
+++ AstronomyPictureOfTheDay/DetailView.swift
@@ -8,8 +8,50 @@
 import SwiftUI
 
 struct DetailView: View {
+    @StateObject var nasaApi = NasaViewModel.shared
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Rectangle()
+                .background(.black)
+                .ignoresSafeArea()
+            
+            VStack {
+                Text(nasaApi.title)
+                    .font(.system(size: 30, weight: .heavy))
+                    .foregroundStyle(.white)
+                
+                GeometryReader { geo in
+                    if nasaApi.mediaType == "video" {
+
+                    } else {
+                        AsyncImage(url: URL(string: nasaApi.sourceUrl ?? "")) { image in
+                            image.resizable()
+                                .frame(width: geo.size.width)
+                                .aspectRatio(contentMode: .fit)
+                        } placeholder: {
+                            ZStack {
+                                Rectangle()
+                                    .frame(width: geo.size.width)
+                            }
+                        }
+                    }
+                } // video or image view
+                
+                ScrollView {
+                    Text(nasaApi.explanation)
+                        .multilineTextAlignment(.leading)
+                        .foregroundStyle(.white)
+                    
+                    HStack {
+                        Spacer()
+                        
+                        Text("photo Ⓒ \(nasaApi.copyright)")
+                            .foregroundStyle(.gray)
+                    } //HStack
+                } //ScrollView
+            } //VStack
+        } //ZStack
     }
 }
 
