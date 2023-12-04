a01dbf44a9c39de3b6d7f2326b857cc560e006dc
Eunice <jes.e.0927@gmail.com>
2023-12-04 22:33:10 +0900

    ✨Feat: Set ContentView - Title, DatePicker, Button

--- AstronomyPictureOfTheDay/ContentView.swift
+++ AstronomyPictureOfTheDay/ContentView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     @State var isMoving: Bool = false
+    @State private var selectedDate = Date()
     
     var body: some View {
         ZStack {
@@ -20,12 +21,44 @@ struct ContentView: View {
                     .animation(.linear(duration: 20).repeatForever(), value: isMoving)
                     .ignoresSafeArea()
             }
-        }
-        .onAppear() {
-            isMoving = true
-        }
-        .onDisappear() {
-            isMoving = false
+            
+            VStack {
+                Spacer()
+                
+                Text("Select a date of \n when you want to see \n the universe")
+                    .font(.system(size: 30, weight: .heavy))
+                    .foregroundStyle(.white)
+                
+                DatePicker("", selection: $selectedDate, in: ...Date(), displayedComponents: .date)
+                    .datePickerStyle(WheelDatePickerStyle())
+                    .labelsHidden()
+                    .colorInvert()
+                
+                Spacer().frame(height: 50)
+                
+                ZStack {
+                    
+                    Button {
+
+                    } label: {
+                        Text("Go")
+                            .frame(width: 83, height: 47)
+                            .background(.black)
+                            .clipShape(RoundedRectangle(cornerRadius: 12))
+                            .foregroundStyle(.white)
+                            .opacity(0.54)
+                            .font(.system(size: 20, weight: .bold))
+                    }
+                }
+                
+                Spacer().frame(height: 100)
+            }
+            .onAppear() {
+                isMoving = true
+            }
+            .onDisappear() {
+                isMoving = false
+            }
         }
     }
 }
