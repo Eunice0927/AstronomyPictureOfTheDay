a21a1eb167fcfa410cba3bedaf4078291fa43379
Eunice <jes.e.0927@gmail.com>
2023-12-04 21:40:32 +0900

    Initial Commit

---
+++ AstronomyPictureOfTheDay/AstronomyPictureOfTheDayApp.swift
@@ -0,0 +1,17 @@
+//
+//  AstronomyPictureOfTheDayApp.swift
+//  AstronomyPictureOfTheDay
+//
+//  Created by Eunsu JEONG on 12/4/23.
+//
+
+import SwiftUI
+
+@main
+struct AstronomyPictureOfTheDayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
