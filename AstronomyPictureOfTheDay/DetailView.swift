80b0310ede4fadfb4c0b14b6223d461aaf2f993c
Eunice <jes.e.0927@gmail.com>
2023-12-04 22:05:47 +0900

    ✨Feat: Create DetailView.swift

---
+++ AstronomyPictureOfTheDay/DetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailView.swift
+//  AstronomyPictureOfTheDay
+//
+//  Created by Eunsu JEONG on 12/4/23.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailView()
+}
