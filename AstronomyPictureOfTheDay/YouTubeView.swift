ff4b2a60d984e11357f16f0126e4cc11f9e881f8
Eunice <jes.e.0927@gmail.com>
2023-12-04 23:06:20 +0900

    ✨Feat: Set YouTubeView with WebKit

---
+++ AstronomyPictureOfTheDay/YouTubeView.swift
@@ -0,0 +1,24 @@
+//
+//  YouTubeVIew.swift
+//  AstronomyPictureOfTheDay
+//
+//  Created by Eunsu JEONG on 12/4/23.
+//
+
+import SwiftUI
+import WebKit
+
+struct YouTubeView: UIViewRepresentable {
+    let videoId: String
+    
+    func makeUIView(context: Context) ->  WKWebView {
+        return WKWebView()
+    }
+    
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        guard let demoURL = URL(string: "https://www.youtube.com/embed/\(videoId)") else { return }
+
+        uiView.scrollView.isScrollEnabled = false
+        uiView.load(URLRequest(url: demoURL))
+    }
+}
